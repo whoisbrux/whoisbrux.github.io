21eeeecf8e0f24f0276153a58f9ba3b8e65c7127
whoisbrux <bruno.efetivos@gmail.com>
2018-02-21 09:26:21 -0300

    Teste

---
+++ js/app.js
@@ -0,0 +1,15 @@
+		tl = new TimelineMax();
+
+ tl.to($slides, 3, {left: "0"}),
+
+
+
+
+			 $('.btn1').on('click',function(){
+			 			tl.play();
+			 });
+
+
+			 			 $('.btn2').on('click',function(){
+			 			tl.reverse();
+			 });
