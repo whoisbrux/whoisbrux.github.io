3f5021fab759fdd091450ea7791826e5c8ccfce8
whoisbrux <whoisbrux@users.noreply.github.com>
2018-02-22 17:23:45 -0300

    17:23

--- js/app.js
+++ js/app.js
@@ -1,34 +1,44 @@
-	
-	TweenMax.to(".home .button", 0, {bottom:"-100px", opacity: "0"});
-	TweenMax.to(".linkMenu", 0, {right:"-100px", opacity: "0"});
-	TweenMax.to(".logoMob", 0, {left:"-100px", opacity: "0"});
-
+	TweenMax.to(".logoMob", 0, {opacity: "0"});
+	TweenMax.to(".linkMenu", 0, {opacity: "0"});
+	TweenMax.to(".home .button", 0, {bottom:"-50px", opacity: "0"});
 
 	TweenMax.to(".intro", 1, {backgroundColor:"#fff"});
-	TweenMax.to(".bordaLogo1", 1.5, {delay:1,width:"150px",opacity:1});
-	TweenMax.to(".bordaLogo2", 1.5, {delay:2,height:"150px", opacity:1});
-	TweenMax.to(".bordaLogo3", 1.5, {delay:1,width:"150px", opacity:1});
-	TweenMax.to(".bordaLogo4", 1.5, {delay:2,height:"150px", opacity:1});
-	TweenMax.to(".backLogo", 1.5, {delay:3.5, bottom:"0px",opacity:1});
+	TweenMax.to(".bordaLogo1", 1.5, {delay:1,width:"100%",opacity:1});
+	TweenMax.to(".bordaLogo2", 1.5, {delay:2,height:"100%", opacity:1});
+	TweenMax.to(".bordaLogo3", 1.5, {delay:1,width:"100%", opacity:1});
+	TweenMax.to(".bordaLogo4", 1.5, {delay:2,height:"100%", opacity:1});
+	TweenMax.to(".backLogo", 1.5, {delay:3.5, ease: Expo.easeInOut, bottom:"0px",opacity:1});
 	TweenMax.to(".cortina", 0, { delay:5, opacity:1});
-	TweenMax.to(".cortina", 3, { delay:6, width:"100%", height:"100%"});	
-	TweenMax.to(".logoMobAnime", 1, {delay:8.5, opacity:.0});
+	TweenMax.to(".cortina", 2, { delay:5.1, ease: Expo.easeOut, width:"100%", height:"100%"});	
+	TweenMax.to(".logoMobAnime", 1, {delay:7, opacity:.0});
+	TweenMax.to(".logoMobAnime", 0, {delay:3.5, color: "#fff"});
+	TweenMax.to(".intro", 3, {delay:7, opacity:.0});
+	TweenMax.to(".textoHome", 1.5, {delay:8.5, y: "-200px", opacity: "1"});
+	TweenMax.to(".logoMob", 1, {delay:10, opacity: "1"});
+	TweenMax.to(".linkMenu", 1, {delay:10, opacity: "1"});
+	TweenMax.to(".home .button", 2, {delay: 10.5,ease: Expo.easeInOut, bottom:"3vh", opacity: "1"});
+	TweenMax.to(".intro", 0, {delay:11.5, display:"none"});
 
-		TweenMax.to(".logoMobAnime", 0, {delay:3.5, color: "#fff"});
 
-  TweenMax.to(".bordaLogo1", 0, {delay:7,width:"70px"});
-	TweenMax.to(".bordaLogo2", 0, {delay:7,height:"70px"});
-	TweenMax.to(".bordaLogo3", 0, {delay:7,width:"70px"});
-	TweenMax.to(".bordaLogo4", 0, {delay:7,height:"70px"});
+	TweenMax.to(".photoSlider", 0, {ease: Expo.easeInOut, right:"-20vw", opacity: "0"});
 
-	TweenMax.to(".intro", 3, {delay:9.5, opacity:.0});
-	TweenMax.to(".textoHome", 1.5, {delay:10, y: "-200px", opacity: "1"});
-	TweenMax.to(".logoMob", 1.5, {delay:11.5, left:"30px", opacity: "1"});
-	TweenMax.to(".linkMenu", 1.5, {delay:11.5, right:"30px", opacity: "1"});
-	TweenMax.to(".home .button", 2, {delay:12,bottom:"30px", opacity: "1"});
+		var tl = new TimelineLite();
+  	tl
+  	tl.pause()
+  	.to(".main", 1.5, {ease: Expo.easeInOut, top:"-100vh"}) 
+    .to(".photoSlider", 1, {ease: Power1.easeInOut, right:"0vw", opacity: "1"});
 	
+	$('.home .button').on('click',function() {
+        tl.play();
+	});
+
+	$('.logoMob').on('click',function() {
+        tl.reverse();
+	});
 
 
-	
+
+
+
 
 
