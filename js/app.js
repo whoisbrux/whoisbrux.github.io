3e0a0dcf896bd31d1e3cf302711a9e1009408a44
whoisbrux <whoisbrux@users.noreply.github.com>
2018-02-27 21:33:28 -0300

    imn2

--- js/app.js
+++ js/app.js
@@ -1,44 +1,23 @@
-	TweenMax.to(".logoMob", 0, {opacity: "0"});
-	TweenMax.to(".linkMenu", 0, {opacity: "0"});
-	TweenMax.to(".home .button", 0, {bottom:"-50px", opacity: "0"});
+	TweenMax.to(".bordaT", 0,{height: "0"});
+	TweenMax.to(".bordaR",0, {width: "0"});
+	TweenMax.to(".bordaB",0, {height: "0"});
+	TweenMax.to(".bordaL",0, {width: "0"});
+	TweenMax.to(".footer",0, {opacity: "0"});
+	TweenMax.to(".logo",0, {y:100, opacity: "0"});
+	TweenMax.to(".tituloContato",0, {y:100,opacity: "0"});	
+	TweenMax.to(".ctnContato",0, {y:100,opacity: "0"});
+	TweenMax.to(".emBreve",0, {y:100,opacity: "0"});	
+	TweenMax.to(".bgPrime",0, {scaleX:1.3, scaleY:1.3, opacity:0});
 
-	TweenMax.to(".intro", 1, {backgroundColor:"#fff"});
-	TweenMax.to(".bordaLogo1", 1.5, {delay:1,width:"100%",opacity:1});
-	TweenMax.to(".bordaLogo2", 1.5, {delay:2,height:"100%", opacity:1});
-	TweenMax.to(".bordaLogo3", 1.5, {delay:1,width:"100%", opacity:1});
-	TweenMax.to(".bordaLogo4", 1.5, {delay:2,height:"100%", opacity:1});
-	TweenMax.to(".backLogo", 1.5, {delay:3.5, ease: Expo.easeInOut, bottom:"0px",opacity:1});
-	TweenMax.to(".cortina", 0, { delay:5, opacity:1});
-	TweenMax.to(".cortina", 2, { delay:5.1, ease: Expo.easeOut, width:"100%", height:"100%"});	
-	TweenMax.to(".logoMobAnime", 1, {delay:7, opacity:.0});
-	TweenMax.to(".logoMobAnime", 0, {delay:3.5, color: "#fff"});
-	TweenMax.to(".intro", 3, {delay:7, opacity:.0});
-	TweenMax.to(".textoHome", 1.5, {delay:8.5, y: "-200px", opacity: "1"});
-	TweenMax.to(".logoMob", 1, {delay:10, opacity: "1"});
-	TweenMax.to(".linkMenu", 1, {delay:10, opacity: "1"});
-	TweenMax.to(".home .button", 2, {delay: 10.5,ease: Expo.easeInOut, bottom:"3vh", opacity: "1"});
-	TweenMax.to(".intro", 0, {delay:11.5, display:"none"});
-
-
-	TweenMax.to(".photoSlider", 0, {ease: Expo.easeInOut, right:"-20vw", opacity: "0"});
-
-		var tl = new TimelineLite();
+var tl = new TimelineLite();
   	tl
-  	tl.pause()
-  	.to(".main", 1.5, {ease: Expo.easeInOut, top:"-100vh"}) 
-    .to(".photoSlider", 1, {ease: Power1.easeInOut, right:"0vw", opacity: "1"});
-	
-	$('.home .button').on('click',function() {
-        tl.play();
-	});
-
-	$('.logoMob').on('click',function() {
-        tl.reverse();
-	});
-
-
-
-
-
-
-
+		  .to(".bgPrime", 3, {scaleX:1.0, scaleY:1.0, opacity:1})
+		  .to(".bordaT", 2,{height: "80px"},"-=2")
+			.to(".bordaR", 2,{width: "80px"},"-=2")
+			.to(".bordaB", 2,{height: "80px"},"-=2")		
+			.to(".bordaL", 2,{width: "80px"},"-=2")	
+			.to(".logo",2, {y:0, opacity: "1.0"},"-=1")
+			.to(".tituloContato",2, {y:0,opacity: "1.0"},"-=1")	
+			.to(".ctnContato",2, {y:0,opacity: "1.0"},"-=1")
+			.to(".emBreve",2, {y:0,opacity: "1.0"},"-=1")
+			.to(".footer",2, {opacity: "1.0"},"-=1");
