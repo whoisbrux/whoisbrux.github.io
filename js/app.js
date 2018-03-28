a7e2b1d94f2a336bb0d1407252abad364c77121a
whoisbrux <whoisbrux@users.noreply.github.com>
2018-03-28 16:30:44 -0300

    teste 27

--- js/app.js
+++ js/app.js
@@ -361,3 +361,15 @@ $('.tm5').on('mousewheel click',function() {
 });
 
 ///////////////////////// HOME ABRE
+
+
+
+ var bodyRect = document.body.getBoundingClientRect(window);
+var www = bodyRect.height
+ 
+$(window).click(function() { 
+  console.log(bodyRect.height)
+});
+
+
+TweenMax.set(".back", { height: www });
