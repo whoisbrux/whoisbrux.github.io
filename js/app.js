f548233f6df0b077e82df698b6fe2e7773fa6303
whoisbrux <whoisbrux@users.noreply.github.com>
2018-03-30 16:59:22 -0300

    up

--- js/app.js
+++ js/app.js
@@ -514,6 +514,25 @@ $(".trg-slide-trg").click(function() {
      goSlideTrg(clickedTrg);
   }
 });
+
+
+///////////////////////// hover menu triangulo
+$(".linktag").hover(
+  function() {
+    var class2 = $(this).attr("class").split(" ")[1];
+    TweenMax.to("."+class2+" img", .3, {opacity: 1, y: 9});
+    console.log("."+class2+" img")
+  },
+  function() {
+    var class2 = $(this).attr("class").split(" ")[1];
+    TweenMax.to("."+class2+" img", .3, {opacity: 0, y:-10});
+   console.log("."+class2+" img")
+  }
+);
+///////////////////////// hover menu triangulo
+
+
+
 ///////////////////////// hover das imagens
 $(".pf-hover").hover(
   function() {
