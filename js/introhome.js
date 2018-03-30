7c345d26706e574bf6a9b0555cd56a56423a7b5e
whoisbrux <whoisbrux@users.noreply.github.com>
2018-03-30 14:02:26 -0300

    up3

--- js/introhome.js
+++ js/introhome.js
@@ -1,8 +1,8 @@
 var tlintroHome = new TimelineMax();
 tlintroHome.from(".cortina", 3, {backgroundColor:"#000"});
 tlintroHome.to(".cortina", 3, { autoAlpha: 0 });
-tlintroHome.from(".trianguloHome", 3,{ ease: Expo.easeInOut, x: -300, autoAlpha: 0 },"-=1.5");
-tlintroHome.from(".tituloHome", 2.5,{ ease: Expo.easeInOut, x: 100, autoAlpha: 0 },"-=2");
+tlintroHome.from(".foto1 .trianguloHome", 3,{ ease: Expo.easeInOut, x: -300, autoAlpha: 0 },"-=1.5");
+tlintroHome.from(".foto1 .tituloHome", 2.5,{ ease: Expo.easeInOut, x: 100, autoAlpha: 0 },"-=2");
 tlintroHome.from(".ctnHeader", 2,{ autoAlpha: 0 },"-=1");
 tlintroHome.from(".trigger", 2,{ ease: Expo.easeInOut, autoAlpha: 0 },"-=2");
-tlintroHome.from(".navegador2", 2,{ ease: Expo.easeInOut,scale: 1.5, autoAlpha: 0 },"-=2");
+tlintroHome.from(".foto1 .navegador2", 2,{ ease: Expo.easeInOut,scale: 1.5, autoAlpha: 0 },"-=2");
