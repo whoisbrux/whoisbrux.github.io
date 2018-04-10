fd44594c6a6e6f9670ff700c654efc12529cb52c
whoisbrux <whoisbrux@users.noreply.github.com>
2018-04-10 15:03:49 -0300

    clean

--- js/introinner.js
+++
@@ -1,6 +0,0 @@
-var tlintroPage = new TimelineMax();
-tlintroPage.from(".cortina", 1, {backgroundColor:"#000"});
-tlintroPage.to(".cortina", 3, { autoAlpha: 0 });
-tlintroPage.from(".i-triangulo", 2,{ ease: Expo.easeInOut, x: -100, autoAlpha: 0 },"-=2");
-tlintroPage.from(".i-title", 2, { ease: Expo.easeInOut, x: 100, autoAlpha: 0 },"-=2");
-tlintroPage.from("button.i-scroll", 2,{ ease: Expo.easeInOut, autoAlpha: 0 },"-=1");
